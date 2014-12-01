8a6cb081a9f767c8ee692081118321fe4226352f
pras_khanal <pras_khanal@hotmail.com>
2014-12-01 11:35:59 +1100

    deleted Unrequired class

--- src/main/java/com/jbpm/demo/dao/ClientDao.java
+++ src/main/java/com/jbpm/demo/dao/ClientDao.java
@@ -1,3 +1,9 @@
+/**
+ * This interface helps to extract information of Clients from Database
+ * as Clients Objects.
+ */
+
+
 package com.jbpm.demo.dao;
 
 import java.util.List;
@@ -5,10 +11,13 @@ import java.util.List;
 import javax.ejb.Local;
 
 import com.jbpm.demo.entity.Client;
-
+/**
+ * 
+ * @author Adam
+ *
+ */
 @Local
 public interface ClientDao {
-
 	List<Client> getAllClient();
 
 	public Client getClient(int clientId);
